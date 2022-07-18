8854f36d2fd2c0607775b6df8253edbdfec5d7a2
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-07-17 23:01:44 -0500

    Impor HashMap

--- retos/Reto2/src/reto2/Reto2.java
+++ retos/Reto2/src/reto2/Reto2.java
@@ -1,4 +1,4 @@
-/*
+import java.util.HashMap;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,6 +16,7 @@ public class Reto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         System.out.println("Hola Reto 1");
     }
     
