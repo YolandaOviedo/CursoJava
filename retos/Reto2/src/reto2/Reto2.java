1ea329c3432651d1e2f587c5aefa14469dd4c648
YolandaOviedo <benavidesjefferson18@gmail.com>
2022-07-17 23:32:44 -0500

    Creación HashMap

--- retos/Reto2/src/reto2/Reto2.java
+++ retos/Reto2/src/reto2/Reto2.java
@@ -1,9 +1,11 @@
-import java.util.HashMap;/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package reto2;
+
+import java.util.HashMap;
 
 /**
  *
@@ -16,8 +18,15 @@ public class Reto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        HashMap<int, ArrayList> listaProductos = new HashMap<int, ArrayList>();
+        listaProductos.put(1, ["Manzanas", 5000.0, 25]);
+        listaProductos.put(2, ["Limones", 2300.0, 15]);
+        listaProductos.put(3, ["Peras", 2700.0, 33]);
+        listaProductos.put(4, ["Arandanos", 9300.0, 5]);
+        
 
-        System.out.println("Hola Reto 1");
+
+        System.out.println(listaProductos);
     }
     
 }
