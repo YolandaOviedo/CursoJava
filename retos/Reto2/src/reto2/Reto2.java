77d050ec158fd6b60f3b36bd90b89a88479d57c2
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-07-19 21:07:28 -0500

    Reto2

--- retos/Reto2/src/reto2/Reto2.java
+++ retos/Reto2/src/reto2/Reto2.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto2;
+package holaJeffer;
 
-import java.util.HashMap;
+//import java.util.HashMap;
 
 /**
  *
@@ -18,15 +18,15 @@ public class Reto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HashMap<int, ArrayList> listaProductos = new HashMap<int, ArrayList>();
-        listaProductos.put(1, ["Manzanas", 5000.0, 25]);
-        listaProductos.put(2, ["Limones", 2300.0, 15]);
-        listaProductos.put(3, ["Peras", 2700.0, 33]);
-        listaProductos.put(4, ["Arandanos", 9300.0, 5]);
-        
+//        HashMap<int, ArrayList> listaProductos = new HashMap<int, ArrayList>();
+//        listaProductos.put(1, ["Manzanas", 5000.0, 25]);
+//        listaProductos.put(2, ["Limones", 2300.0, 15]);
+//        listaProductos.put(3, ["Peras", 2700.0, 33]);
+//        listaProductos.put(4, ["Arandanos", 9300.0, 5]);
+//        
 
 
-        System.out.println(listaProductos);
+        System.out.println("Hola");
     }
     
 }
