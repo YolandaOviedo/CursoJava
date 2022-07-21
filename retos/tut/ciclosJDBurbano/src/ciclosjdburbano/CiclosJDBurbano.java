52c3a3f4816c71a999cfb42afc1a4f45e9fa2e46
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-07-21 07:43:58 -0500

    Solucionando fallas del pull

--- retos/tut/ciclosJDBurbano/src/ciclosjdburbano/CiclosJDBurbano.java
+++ retos/tut/ciclosJDBurbano/src/ciclosjdburbano/CiclosJDBurbano.java
@@ -1,5 +1,6 @@
 /*
  * TUTORÍAS SOBRE BUCLES 
+ * HAY FALLAS EN LOS GIT PULL
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
